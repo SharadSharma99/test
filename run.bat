5847b282e7ade0b28242ab8eb46114ec2b55fc09
SharadSharma99 <39654729+SharadSharma99@users.noreply.github.com>
2018-05-27 23:25:56 +0530

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-Java -cp "C:\Jenkins\LTMS_Functional_Automation_Suite\lib\*;C:\Jenkins\LTMS_Functional_Automation_Suite\bin" org.testng.TestNG C:\Jenkins\LTMS_Functional_Automation_Suite\testng.xml
+Java -cp "C:\BuildAgent\work\4cf23f1581bb7c12\lib\*;C:\BuildAgent\work\4cf23f1581bb7c12\bin" org.testng.TestNG CC:\BuildAgent\work\4cf23f1581bb7c12\testng.xml
